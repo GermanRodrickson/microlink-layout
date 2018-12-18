c202385796ad4541a5143f453200b5cd4496bd35
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-18 21:38:20 +0100

    base

---
+++ src/Microlinkcard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class Microlinkcard extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default Microlinkcard
