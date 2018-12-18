9411744ade0502936bfcc8f2e7a55a48dff1073e
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-18 21:44:09 +0100

    microlinkcard

--- src/Microlinkcard.js
+++ src/Microlinkcard.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  width: 450px;
+  height: 250px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+`;
 
 
 class Microlinkcard extends Component {
   render() {
-    return (
-      <div>Hello</div>
-    )
+    return <Wrapper>import MicrolinkCard from 'react-microlink'</Wrapper>;
   }
 }
 
