4b5c7b1ab8b1c9d793927c6e12a82a26bc95b53d
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-19 06:58:17 +0100

    placement

--- src/Components/Logos.js
+++ src/Components/Logos.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 //Images
 import Applelogo from '../img/apple-logo.png';
@@ -8,11 +8,10 @@ import Youtubelogo from '../img/youtube-logo.png';
 import Instragramlogo from '../img/instagram-logo.png';
 
 const Wrapper = styled.section`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    max-width: 1100px;
-    padding-top: 2%;
+  padding-top: 2%;
+  position: relative;
+  display: flex;
+  justify-content: center;
 
   img {
     width: 50px;
@@ -20,43 +19,48 @@ const Wrapper = styled.section`
     border-radius: 100%;
 
     &:hover {
+      will-change: transform;
       transform: scale(1.2, 1.2);
-      padding: 20px;
+      padding: 10px;
     }
   }
 `;
 
+
+const Apple = styled.img`
+  margin-bottom: 2.5%;
+`;
+const Medium = styled.img`
+  margin-bottom: 2.5%;
+  margin-left: 50px;
+`;
+const Youtube = styled.img`
+  margin-left: -50px;
+`;
+const Instagram = styled.img`
+`;
+
+
+
+
 class App extends Component {
   render() {
     return (
-      <Wrapper>
-        <ul>
-          <li><img src={Applelogo} alt="apple-logo" /></li>
-          <li><img src={Mlogo} alt="m-logo" /></li>
-          <li><img src={Youtubelogo} alt="youtube-logo" /></li>
-          <li><img src={Instragramlogo} alt="instagram-logo" /></li>
-        </ul>
-        {/* <img src={Applelogo} alt="apple-logo"/>
-        <img src={Mlogo} alt="m-logo"/>
-        <img src={Youtubelogo} alt="youtube-logo"/>
-        <img src={Instragramlogo} alt="instagram-logo"/>
-        <img src={Applelogo} alt="apple-logo"/>
-        <img src={Mlogo} alt="m-logo"/>
-        <img src={Youtubelogo} alt="youtube-logo"/>
-        <img src={Instragramlogo} alt="instagram-logo"/>
-        <img src={Applelogo} alt="apple-logo"/>
-        <img src={Mlogo} alt="m-logo"/>
-        <img src={Youtubelogo} alt="youtube-logo"/>
-        <img src={Instragramlogo} alt="instagram-logo"/>
-        <img src={Applelogo} alt="apple-logo"/>
-        <img src={Mlogo} alt="m-logo"/>
-        <img src={Youtubelogo} alt="youtube-logo"/>
-        <img src={Instragramlogo} alt="instagram-logo"/>
-        <img src={Applelogo} alt="apple-logo"/>
-        <img src={Mlogo} alt="m-logo"/>
-        <img src={Youtubelogo} alt="youtube-logo"/>
-        <img src={Instragramlogo} alt="instagram-logo"/> */}
-      </Wrapper>
+      <section>
+        
+        
+        <Wrapper>
+          <ul>
+            <li><Apple src={Applelogo} alt="apple-logo" /></li>
+            <li><Medium src={Mlogo} alt="m-logo" /></li>
+            <li><Youtube src={Youtubelogo} alt="youtube-logo" /></li>
+            <li><Instagram src={Instragramlogo} alt="instagram-logo" /></li>
+          </ul>
+        </Wrapper>
+        
+        
+      </section>
+      
     )
   }
 }
